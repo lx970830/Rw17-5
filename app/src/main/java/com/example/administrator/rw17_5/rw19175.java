a0acc25978c273aff4e580e202e1c909695f741c
lx970830 <1539619936@qq.com>
2018-11-29 16:42:37 +0800

    第一次

---
+++ app/src/main/java/com/example/administrator/rw17_5/rw19175.java
@@ -0,0 +1,13 @@
+package com.example.administrator.rw17_5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rw19175 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rw19175);
+    }
+}
