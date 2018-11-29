d98828fa95bf7d5ba4f50ff641a5eefd50dcf1b5
lx970830 <1539619936@qq.com>
2018-11-29 16:54:41 +0800

    第二次

--- app/src/main/java/com/example/administrator/rw17_5/MainActivity.java
+++ app/src/main/java/com/example/administrator/rw17_5/MainActivity.java
@@ -3,15 +3,11 @@ package com.example.administrator.rw17_5;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class rw19175 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rw19175);
-        in();
-    }
-
-    private void in() {
+        setContentView(R.layout.activity_main);
     }
 }
